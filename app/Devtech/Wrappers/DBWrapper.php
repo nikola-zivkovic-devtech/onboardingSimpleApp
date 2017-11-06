67bba6f4bafe27a4614f21d979d8ff2895e7ca81
Nikola Zivkovic <nikola.zivkovic@devtechgroup.com>
2017-11-06 14:19:45 +0100

    Added database selection wrapper.

---
+++ app/Devtech/Wrappers/DBWrapper.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikola.zivkovic
+ * Date: 06-Nov-17
+ * Time: 11:52
+ */
+
+namespace Devtech\Wrappers;
+
+
+class DBWrapper
+{
+    private $database;
+
+    public function __construct($database) {
+        $class = $database . "Database";
+        $this->database = new $class();
+    }
+
+    public function find($query) {
+        $this->database->find($query);
+    }
+
+    public function write($query) {
+        $this->database->write($query);
+    }
+
+    public function delete($message) {
+        $this->database->delete($message);
+    }
+}
