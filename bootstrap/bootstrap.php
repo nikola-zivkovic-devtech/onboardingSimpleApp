3d96ea86bbcc82df329d3a41b1777aae650dad49
Nikola Zivkovic <nikola.zivkovic@devtechgroup.com>
2017-11-09 10:20:12 +0100

    Added /sofa routes, updated readme.md.

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -13,6 +13,8 @@ $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r)
     $r->addRoute('GET', '/[index]', 'Welcome');
     $r->addRoute('GET', '/store/chair', 'StoreController@getAll');
     $r->addRoute('GET', '/store/chair/{id:[0-9]+}', 'StoreController@getOne');
+    $r->addRoute('GET', '/store/sofa', 'StoreController@getAll');
+    $r->addRoute('GET', '/store/sofa/{id:[0-9]+}', 'StoreController@getOne');
 });
 
 $db = new Database();
